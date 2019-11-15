57a609f0025e54d8bb9d9a9cde81664768d389e8
Sean <sean@hamistudios.com>
2019-11-15 08:40:36 +0000

    chore(*): initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// npm
+// const core = require('@actions/core')
+const github = require('@actions/github')
+
+// self
+// const issue = require('./issue')
+
+console.log(JSON.stringify(github.context))
